1adf37e5db0c20de3391b4e6fbc0e6926d40b321
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2015-04-02 11:53:22 +0200

    Initial test version

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='check_sub10_e1000',
+      version='0.10',
+      description='Nagios Check for sub10 Systems Liberator E1000 ',
+      author='Dr. Torge Szczepanek',
+      author_email='info@cygnusnetworks.de',
+      license='GPL',
+      packages=['sub10_e1000_snmp'],
+      scripts=['check_sub10_e1000'],
+      install_requires=['nagiosplugin', 'pysnmp']
+	)
